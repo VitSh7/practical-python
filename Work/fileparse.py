2c2bd7509bc0d68ca6956ab1d0d51e0f840411c6
Vitaly Shmagun <65466721+Vitve4@users.noreply.github.com>
2023-11-27 19:05:35 +0100

    add fileparse

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,51 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
+    """
+    Parse a CSV file into a list of records
+    """
+    if select is None:
+        select = ['name', 'shares', 'price']
+    with open(filename) as f:
+        rows = csv.reader(f, delimiter=delimiter)
+
+        # Read the file headers
+        indices = []
+        if has_headers:
+            headers = next(rows)
+            if select:
+                indices = [headers.index(h) for h in headers if h in select]
+                headers = select
+
+
+
+        records = []
+        for row in rows:
+            if not row:    # Skip rows with no data
+                continue
+
+            if indices:
+                row = [row[i] for i in indices]
+
+            if types:
+                row = [func(item) for func, item in zip(types,row)]
+
+            if has_headers:
+                record = dict(zip(headers, row))
+            else:
+                record = tuple(row)
+            records.append(record)
+
+    return records
+
+
+# portfolio = parse_csv('Data/portfolio.csv', select=["name", "shares"], types=[str, int])
+prices= parse_csv('Data/prices.csv', types=[str, float], has_headers=False)
+portfolio = parse_csv('Data/portfolio.dat', types=[str, int, float], delimiter=' ')
+print(prices)
+print(portfolio)
