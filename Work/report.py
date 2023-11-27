ceccdb3324a88b4c1c5b4a0f552bf5535852364a
Vitaly Shmagun <65466721+Vitve4@users.noreply.github.com>
2023-11-27 17:21:11 +0100

    add exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,51 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+
+def read_portfolio(filename):
+    portfolio = []
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
+            portfolio.append(holding)
+
+    return portfolio
+
+
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            if len(row) > 1:
+                prices[row[0]] = float(row[1])
+
+    return prices
+
+
+def calculate_gains(portfolio, prices):
+    for stock in portfolio:
+        if stock["name"] in prices:
+            change = prices[stock["name"]] - stock["price"]
+            stock["price"] = prices[stock["name"]]
+            stock["gain"] = change * stock["shares"]
+
+
+def print_table(table):
+    print('{:^10s} {:^10s} {:^10s} {:^10s}'.format('Name', "Shares", "Price", "Gain"))
+    print('{:^10s} {:^10s} {:^10s} {:^10s}'.format('------', "------", "------",
+                                                   "------"))
+    for row in table:
+        print('{name:<10s} {shares:^10d} {price:^10.2f} {gain:^10.2f}'.format_map(row))
+
+
+Portfolio = read_portfolio("Data/portfolio.csv")
+Prices = read_prices("Data/prices.csv")
+calculate_gains(Portfolio, Prices)
+
+print_table(Portfolio)
