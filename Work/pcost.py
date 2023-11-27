8ae951b41de1500c404f9cc5dc388ae3eb4367ce
Vitaly Shmagun <65466721+Vitve4@users.noreply.github.com>
2023-11-27 14:15:45 +0100

    add exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+
+f = open("Data/portfolio.csv", "rt")
+
+headers = next(f).split(",")
+
+name = []
+shares = []
+price = []
+
+
+for line in f:
+    row = line.split(',')
+    name.append(row[0])
+    shares.append(int(row[1]))
+    price.append(float(row[2]))
+
+sum = 0
+for s, p in zip(shares,price):
+    sum += s*p
+
+print("Total Price: ", sum)
+
+f.close()
